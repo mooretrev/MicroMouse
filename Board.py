38b9d3fd11478e62e08f7223caa902e6427f0810
Dean Orenstein <dto3576@gmail.com>
2019-11-08 16:20:50 -0600

    edited board

---
+++ Board.py
@@ -0,0 +1,14 @@
+'''
+Board class
+Boolean matrix to show where walls and open spots are
+tape = 1 = true, tape = 0 = false
+Create mazes based on input file given
+Display walls
+'''
+import numpy
+
+class CreateBoard:
+    def __init__(self):
+        
+
+
